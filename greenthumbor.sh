3810aa2bd1a215e7804933d59df90799662c35f5
Justin Bowen <JusBowen@gmail.com>
2019-01-29 22:29:43 -0800

    Moving thumbor greenthumb filter

--- greenthumbor.sh
+++ greenthumbor.sh
@@ -1,4 +1,4 @@
-cd /greenthumb-image-api/thumbor-greenthumb-filter
+cd /thumbor-greenthumb-filter
 python setup.py bdist_wheel
 python -m pip install dist/thumbor_greenthumb_filter-0.0.4-py2-none-any.whl --upgrade
-thumbor -p 3030 -c ../thumbor.conf
+thumbor -p 3030 -c /greenthumb-image-api/thumbor.conf
